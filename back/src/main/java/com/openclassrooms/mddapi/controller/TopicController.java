a9171f663cd5251bfb23d8ec5b6c9c6c8e2c1d41
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2026-02-05 13:45:06 +0100

    feat(topic): :sparkles: expose endpoints for topic create and update

--- back/src/main/java/com/openclassrooms/mddapi/controller/TopicController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/TopicController.java
@@ -2,15 +2,23 @@ package com.openclassrooms.mddapi.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.openclassrooms.mddapi.model.Topic;
+import com.openclassrooms.mddapi.dto.TopicDto;
+import com.openclassrooms.mddapi.payload.response.MessageResponse;
 import com.openclassrooms.mddapi.service.ITopicService;
 
+import jakarta.validation.Valid;
+
 @RestController
-@RequestMapping("/topic")
+@RequestMapping("/topics")
 public class TopicController {
 	
 	private ITopicService topicService;
@@ -20,9 +28,27 @@ public class TopicController {
 	}
 
 	@GetMapping
-	public List<Topic> getTopics() {
+	public List<TopicDto> getTopics() {
 		return topicService.getTopics();
 	}
-	
+
+	@PostMapping
+	public ResponseEntity<MessageResponse> createTopic(@Valid @RequestBody TopicDto request) {
+		topicService.createTopic(request);
+
+		return ResponseEntity.status(201)
+			.body(new MessageResponse("Topic created successfully!"));
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<MessageResponse> updateTopic(
+		@PathVariable Long id, 
+		@Valid @RequestBody TopicDto request
+	) {
+		topicService.updateTopic(id, request);
+		
+		return ResponseEntity.ok()
+			.body(new MessageResponse("Topic updated successfully!"));
+	}
 	
 }
