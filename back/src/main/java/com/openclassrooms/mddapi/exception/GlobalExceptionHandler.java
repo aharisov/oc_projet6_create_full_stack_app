5e44f5b622bdad7df562a7adaad2a054e4e0d405
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2026-02-05 14:19:39 +0100

    feat(global): :sparkles: add custom exception handlers method not allowed, route not found

--- back/src/main/java/com/openclassrooms/mddapi/exception/GlobalExceptionHandler.java
+++ back/src/main/java/com/openclassrooms/mddapi/exception/GlobalExceptionHandler.java
@@ -3,9 +3,11 @@ package com.openclassrooms.mddapi.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.openclassrooms.mddapi.payload.response.MessageResponse;
 
@@ -46,5 +48,17 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<MessageResponse> handleGeneric(Exception ex) {
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
 			.body(new MessageResponse("Unexpected error"));
+	}
+
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	public ResponseEntity<MessageResponse> handleMethodNotSupported(HttpRequestMethodNotSupportedException ex) {
+		return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
+			.body(new MessageResponse("Method not allowed"));
+	}
+
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public ResponseEntity<MessageResponse> handleNoHandlerFoundException(NoHandlerFoundException ex) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+			.body(new MessageResponse("Route not found"));
 	}
 }
