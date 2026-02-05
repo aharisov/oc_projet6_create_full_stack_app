445a56c85d0400cd6dbae07421676274790b7642
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2026-02-05 16:59:34 +0100

    feat(post): :sparkles: add required fields to post model

--- back/src/main/java/com/openclassrooms/mddapi/model/Post.java
+++ back/src/main/java/com/openclassrooms/mddapi/model/Post.java
@@ -1,5 +1,10 @@
 package com.openclassrooms.mddapi.model;
 
+import java.time.Instant;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,7 +13,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "posts")
 public class Post {
@@ -19,26 +26,24 @@ public class Post {
 	private Long id;
 	
 	@ManyToOne
-	@JoinColumn(name = "topic_id")
+	@JoinColumn(name = "topic_id", nullable = false)
 	private Topic topic;
+
+	@ManyToOne
+	@JoinColumn(name = "author_id", nullable = false)
+	private User author;
 	
-	// TODO : to finish...
+	@Column(nullable = false, length = 200)
+	private String title;
 
-	public Long getId() {
-		return id;
-	}
+	@Column(columnDefinition = "LONGTEXT", nullable = false)
+	private String content;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Topic getTopic() {
-		return topic;
-	}
-
-	public void setTopic(Topic topic) {
-		this.topic = topic;
-	}
-		
-	
+	@Column(name="created_at", columnDefinition = "TIMESTAMP")
+    @CreationTimestamp
+    private Instant createdAt;
+    
+    @Column(name="updated_at", columnDefinition = "TIMESTAMP")
+    @UpdateTimestamp
+    private Instant updatedAt;
 }
