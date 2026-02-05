f5d78b5d25dd791d996a29cafd807f170456dc18
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2026-02-05 17:01:15 +0100

    feat(post): :sparkles: update post repository with custom methods

--- back/src/main/java/com/openclassrooms/mddapi/repository/PostRepository.java
+++ back/src/main/java/com/openclassrooms/mddapi/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.openclassrooms.mddapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.openclassrooms.mddapi.model.Post;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>{
-
+	List<Post> findAllByOrderByCreatedAtDesc();
+	List<Post> findAllByOrderByCreatedAtAsc();
 }
