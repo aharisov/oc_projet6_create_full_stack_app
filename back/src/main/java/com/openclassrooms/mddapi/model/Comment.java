478d7c37d87000c4e2f743642ee0d59c5eee2ec2
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2026-02-06 17:47:45 +0100

    feat(comment): :sparkles: update comment model with new fields

--- back/src/main/java/com/openclassrooms/mddapi/model/Comment.java
+++ back/src/main/java/com/openclassrooms/mddapi/model/Comment.java
@@ -1,12 +1,20 @@
 package com.openclassrooms.mddapi.model;
 
+import java.time.Instant;
+
+import org.hibernate.annotations.CreationTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "comments")
 public class Comment {
@@ -15,15 +23,19 @@ public class Comment {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "comment_id")
 	private Long id;
-	
-	// TODO : to finish...
 
-	public Long getId() {
-		return id;
-	}
+	@ManyToOne
+	@JoinColumn(name = "post_id", nullable = false)
+	private Post post;
 
-	public void setId(Long id) {
-		this.id = id;
-	}	
-	
+	@ManyToOne
+	@JoinColumn(name = "author_id", nullable = false)
+	private User author;
+
+	@Column(columnDefinition = "TEXT", nullable = false)
+	private String content;
+
+	@CreationTimestamp
+	@Column(name = "created_at", columnDefinition = "TIMESTAMP")
+	private Instant createdAt;
 }
