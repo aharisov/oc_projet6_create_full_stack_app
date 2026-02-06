d980b87457431239f613e11ff543bc116fe54e77
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2026-02-06 19:51:34 +0100

    feat(comment): :sparkles: add comment dto with validation, comment mapper

---
+++ back/src/main/java/com/openclassrooms/mddapi/mapper/CommentMapper.java
@@ -0,0 +1,41 @@
+package com.openclassrooms.mddapi.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.openclassrooms.mddapi.dto.CommentDto;
+import com.openclassrooms.mddapi.model.Comment;
+import com.openclassrooms.mddapi.model.Post;
+import com.openclassrooms.mddapi.model.User;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper extends EntityMapper<CommentDto, Comment> {
+
+	@Override
+	@Mapping(target = "post", expression = "java(toPost(dto.getPostId()))")
+	@Mapping(target = "author", expression = "java(toUser(dto.getAuthorId()))")
+	Comment toEntity(CommentDto dto);
+
+	@Override
+	@Mapping(target = "postId", source = "post.id")
+	@Mapping(target = "authorId", source = "author.id")
+	CommentDto toDto(Comment entity);
+
+	default Post toPost(Long id) {
+		if (id == null) {
+			return null;
+		}
+		Post post = new Post();
+		post.setId(id);
+		return post;
+	}
+
+	default User toUser(Long id) {
+		if (id == null) {
+			return null;
+		}
+		User user = new User();
+		user.setId(id);
+		return user;
+	}
+}
