96aff32b2aa8307f87b70b0e6cacd3c18b76eb93
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2026-02-12 16:49:22 +0100

    fix(post): :sparkles: update post repository to support filter by user subscriptions

--- back/src/main/java/com/openclassrooms/mddapi/repository/PostRepository.java
+++ back/src/main/java/com/openclassrooms/mddapi/repository/PostRepository.java
@@ -1,14 +1,26 @@
 package com.openclassrooms.mddapi.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.openclassrooms.mddapi.model.Post;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>{
-	List<Post> findAllByOrderByCreatedAtDesc();
-	List<Post> findAllByOrderByCreatedAtAsc();
+	@Query("SELECT p FROM Post p JOIN Subscription s ON s.topic.id = p.topic.id "
+		+ "WHERE s.user.id = :userId ORDER BY p.createdAt DESC")
+	List<Post> findAllSubscribedByUserIdOrderByCreatedAtDesc(@Param("userId") Long userId);
+
+	@Query("SELECT p FROM Post p JOIN Subscription s ON s.topic.id = p.topic.id "
+		+ "WHERE s.user.id = :userId ORDER BY p.createdAt ASC")
+	List<Post> findAllSubscribedByUserIdOrderByCreatedAtAsc(@Param("userId") Long userId);
+
+	@Query("SELECT p FROM Post p JOIN Subscription s ON s.topic.id = p.topic.id "
+		+ "WHERE p.id = :postId AND s.user.id = :userId")
+	Optional<Post> findSubscribedPostByIdAndUserId(@Param("postId") Long postId, @Param("userId") Long userId);
 }
