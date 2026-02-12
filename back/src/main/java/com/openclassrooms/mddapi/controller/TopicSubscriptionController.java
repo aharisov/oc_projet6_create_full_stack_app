f5dc8311f3255f84742a6b91f194498109d62a98
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2026-02-12 16:48:28 +0100

    feat(subscription): :necktie: update subscription to support getting subscribed topics

--- back/src/main/java/com/openclassrooms/mddapi/controller/TopicSubscriptionController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/TopicSubscriptionController.java
@@ -1,12 +1,16 @@
 package com.openclassrooms.mddapi.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.openclassrooms.mddapi.dto.TopicDto;
 import com.openclassrooms.mddapi.payload.response.MessageResponse;
 import com.openclassrooms.mddapi.service.ISubscriptionService;
 
@@ -18,6 +22,11 @@ public class TopicSubscriptionController {
 
 	public TopicSubscriptionController(ISubscriptionService subscriptionService) {
 		this.subscriptionService = subscriptionService;
+	}
+
+	@GetMapping("/subscribed")
+	public List<TopicDto> getSubscribedTopics() {
+		return subscriptionService.getSubscribedTopics();
 	}
 
 	@PostMapping("/{id}/subscribe")
