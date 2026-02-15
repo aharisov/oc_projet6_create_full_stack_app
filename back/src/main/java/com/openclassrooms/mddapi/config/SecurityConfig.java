01b583b5e66c495567ebbbe01849906ca794483f
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2026-02-15 16:37:21 +0100

    docs(swagger): :wrench: update config to support swagger

--- back/src/main/java/com/openclassrooms/mddapi/config/SecurityConfig.java
+++ back/src/main/java/com/openclassrooms/mddapi/config/SecurityConfig.java
@@ -20,7 +20,12 @@ public class SecurityConfig {
 			.csrf(csrf -> csrf.disable())
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(auth -> auth
-				.requestMatchers("/auth/**").permitAll() 
+				.requestMatchers(
+					"/auth/**",
+					"/v3/api-docs/**",
+					"/swagger-ui/**",
+					"/swagger-ui.html"
+				).permitAll()
 				.anyRequest().authenticated()
 			)
 			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
