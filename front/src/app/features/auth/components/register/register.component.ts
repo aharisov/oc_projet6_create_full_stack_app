266a142bb5b00077d9189fe55037e9d79d8ee2ed
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2026-02-19 14:26:50 +0100

    feat(front-auth): :sparkles: add blank login and register pages

---
+++ front/src/app/features/auth/components/register/register.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+  standalone: false
+})
+export class RegisterComponent {}
