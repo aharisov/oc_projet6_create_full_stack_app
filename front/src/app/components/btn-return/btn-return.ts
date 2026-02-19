1df63158fe048d50baa44994cec71d89233d53a0
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2026-02-19 16:59:40 +0100

    feat(front-common): :sparkles: add common return button component

---
+++ front/src/app/components/btn-return/btn-return.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-btn-return',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './btn-return.html',
+  styleUrl: './btn-return.css',
+})
+export class BtnReturn {
+
+}
