cce5bf61743fea0be1ecbba262262f6d63aaf9a7
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2026-02-27 11:10:12 +0100

    refactor(post): :recycle: update post item to support post details open

--- front/src/app/features/posts/components/post-item/post-item.component.ts
+++ front/src/app/features/posts/components/post-item/post-item.component.ts
@@ -1,12 +1,13 @@
 import { DatePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 import { Post } from 'src/app/interfaces/post.interface';
 
 @Component({
   selector: 'app-post-item',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, RouterLink],
   templateUrl: './post-item.component.html',
   styleUrl: './post-item.component.css'
 })
