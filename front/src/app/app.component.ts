d34b79dc6295a8ceb24e3f6c5d2deadfd94ce60f
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2026-02-27 14:55:59 +0100

    chore(front-common): :recycle: update app component to standalone mode

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { HeaderComponent } from './components/header/header';
 import { AuthService } from './features/auth/services/auth.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    standalone: false
+    standalone: true,
+    imports: [HeaderComponent, RouterOutlet]
 })
 export class AppComponent implements OnInit {
   private readonly authService = inject(AuthService);
