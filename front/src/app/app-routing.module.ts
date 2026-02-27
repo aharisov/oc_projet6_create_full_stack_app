f16cf010bfc08191ccc535e73380db8be14b63ab
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2026-02-27 11:10:35 +0100

    feat(post): :sparkles: add post details page

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -8,10 +8,12 @@ import { guestOnlyGuard } from './features/auth/guards/guest-only.guard';
 import { authRequiredGuard } from './features/auth/guards/auth-required.guard';
 import { TopicsPageComponent } from './features/topics/pages/topics-page/topics-page.component';
 import { PostsPageComponent } from './features/posts/pages/posts-page/posts-page.component';
+import { PostDetailsPageComponent } from './features/posts/pages/post-details-page/post-details-page.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [guestOnlyGuard] },
   { path: 'posts', component: PostsPageComponent, canActivate: [authRequiredGuard] },
+  { path: 'posts/:id', component: PostDetailsPageComponent, canActivate: [authRequiredGuard] },
   { path: 'topics', component: TopicsPageComponent, canActivate: [authRequiredGuard] },
   { path: 'login', component: LoginComponent, canActivate: [guestOnlyGuard] },
   { path: 'register', component: RegisterComponent, canActivate: [guestOnlyGuard] },
