8a2c638f219eafb3e1ae5d132fc3e88ca7d78421
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2026-02-27 14:55:05 +0100

    chore(front-common): :recycle: update home component to standalone mode

--- front/src/app/features/home/home.component.ts
+++ front/src/app/features/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  standalone: false
+  standalone: true,
+  imports: [RouterLink]
 })
 export class HomeComponent {
 }
